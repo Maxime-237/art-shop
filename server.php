e983fa367b5a1257068f6b24d6e39292ca166e7c
Maxime-237 <enfantposemaxime@gmail.com>
2026-05-10 03:11:42 +0100

    Add server.php for Railway

---
+++ server.php
@@ -0,0 +1,13 @@
+<?php
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
+
+$_SERVER['SCRIPT_FILENAME'] = __DIR__.'/public/index.php';
+
+require_once __DIR__.'/public/index.php';
