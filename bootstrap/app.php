a61dcfc2ee27f1c0e462092b9fcf132c6a8951d8
Maxime-237 <enfantposemaxime@gmail.com>
2026-05-10 06:02:51 +0100

    Register CheckRole middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,12 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // ← Ajouter cette ligne
+        $middleware->alias([
+            'role' => \App\Http\Middleware\CheckRole::class,
+        ]);
     })
-    ->withMiddleware(fn($m) => $m->alias([
-        'role' => \App\Http\Middleware\CheckRole::class
-    ]))
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
